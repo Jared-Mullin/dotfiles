4f6844d2d5d4af84f1a48b4085ec0a1879ebdd70
Jared Mullin <ori.mullin@gmail.com>
2024-03-31 00:29:12 +0100

    Reverse the symlink for Tmux + Nvim:

---
+++ nvim/lua/general.lua
@@ -0,0 +1,15 @@
+-- use indents of 2
+vim.opt.shiftwidth = 2
+
+-- tabs are tabs
+vim.opt.expandtab = false
+
+-- an indentation every 2 columns
+vim.opt.tabstop = 2
+
+-- let backspace delete indent
+vim.opt.softtabstop = 2
+
+-- enable smart indentation
+vim.opt.smartindent = true
+
---
+++ nvim/lua/ui.lua
@@ -0,0 +1,13 @@
+-- line numbers
+vim.wo.number = true
+
+-- show matching brackets/parenthesis
+vim.opt.showmatch = true
+
+-- highlight cursor
+vim.opt.cursorline = true
+
+-- syntax highlighting
+vim.opt.termguicolors = true
+vim.opt.synmaxcol = 512
+
