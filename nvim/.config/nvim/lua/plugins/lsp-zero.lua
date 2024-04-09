4e2387f05a2262e93bde069165d100103aa9b88e
Jared Mullin <ori.mullin@gmail.com>
2024-04-09 21:09:24 +0200

    Add yamlls

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -19,6 +19,7 @@ return {
             ensure_installed = {
                 'lua_ls',
                 'gopls',
+                'yamlls',
             },
             handlers = {
                 function(server_name)
