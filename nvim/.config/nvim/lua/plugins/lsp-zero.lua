24f604015b1b93f76d21283612893d48e0726b3b
Jared-Mullin <Jaredmullin@protonmail.com>
2024-04-12 21:23:41 +0100

    Update lsp-zero.lua

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -27,5 +27,13 @@ return {
                 end,
             },
         })
+
+        -- format on save
+        vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = buffer,
+                callback = function()
+                    vim.lsp.buf.format { async = false }
+                end
+            })
     end
 }
